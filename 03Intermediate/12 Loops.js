72acbd5e6be6187ce0c0776e726cdf8c700be410
Sourav <sourav@POSITRAs-Mac-mini.local>
2023-03-10 11:01:38 +0530

    for,while,do while loops

---
+++ 03Intermediate/12 Loops.js
@@ -0,0 +1,32 @@
+// for(let i = 0; i <= 10; i++){
+//   console.log(i);
+// }
+
+const myState = [
+    "Meghalaya", 
+    "Delhi", 
+    "Rajasthan", 
+    "Assam", 
+    1947,
+    "Mizoram", 
+    "Arunachal Pradesh"
+];
+
+// for(let i = 0; i<myState.length; i++) {
+//   if(typeof myState[i] !== "string") continue;
+//   console.log(myState[i]);
+// }
+
+// let i = 0;
+
+// while(i<myState.length){
+//   console.log(myState[i]);
+//   i++;
+// }
+let i = 20;
+
+do{
+  console.log(i);
+  i++;
+}while(i<10)
+
