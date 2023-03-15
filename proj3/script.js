ca6042e45dc25dbc477239ffde27e74f630f4c94
Sourav <sourav.choudhury@positra.com>
2023-03-15 17:02:29 +0530

    generate elements

---
+++ proj3/script.js
@@ -0,0 +1,26 @@
+const courses = [
+  {
+    name : "Complete ReactJS course",
+    price : "$2.3"
+  },
+  {
+    name : "Complete Angular course",
+    price : "$2.1"
+  },
+  {
+    name : "Complete MERN course",
+    price : "$2.7"
+  },
+  {
+    name : "Complete C++ course",
+    price : "$2.8"
+  },
+];
+
+function generateLIST() {
+  const ul = document.querySelector(".list-group");
+  courses.forEach(course => {
+
+    
+  })
+}
