41ac2fd340f5843f1c911516862ce8edf4267b7e
Sourav <sourav.choudhury@positra.com>
2023-03-16 11:16:07 +0530

    Template literals in javascript

---
+++ 06 AdvanceIsh/02 strings.js
@@ -0,0 +1,8 @@
+var name = `sourav`;
+var fullName = `${name} choudhury`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));  
+console.log(fullName.endsWith("y"));  
+console.log(fullName.includes("sourav"));  
+console.log(fullName.toUpperCase());  
