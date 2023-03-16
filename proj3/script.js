5f433d19106d76b3433692f2b03ed356e3e97633
Sourav <sourav.choudhury@positra.com>
2023-03-16 10:48:01 +0530

    Generating elements and Text node in DOM

--- proj3/script.js
+++ proj3/script.js
@@ -1,26 +1,56 @@
 const courses = [
   {
-    name : "Complete ReactJS course",
-    price : "$2.3"
+    name: "Complete ReactJS course",
+    price: "2.3",
   },
   {
-    name : "Complete Angular course",
-    price : "$2.1"
+    name: "Complete Angular course",
+    price: "2.1",
   },
   {
-    name : "Complete MERN course",
-    price : "$2.7"
+    name: "Complete MERN course",
+    price: "2.7",
   },
   {
-    name : "Complete C++ course",
-    price : "$2.8"
+    name: "Complete C++ course",
+    price: "2.8",
   },
 ];
 
 function generateLIST() {
   const ul = document.querySelector(".list-group");
-  courses.forEach(course => {
+  ul.innerHTML = "";
+  courses.forEach((course) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
 
-    
-  })
+    const name = document.createTextNode(course.name);
+    li.appendChild(name);
+
+    const span = document.createElement("span");
+    span.classList.add("float-right");
+    const price = document.createTextNode(`$${course.price}`);
+    span.appendChild(price);
+
+    li.appendChild(span);
+    ul.appendChild(li);
+  });
 }
+
+// generateLIST();
+
+window.addEventListener("load", generateLIST);
+
+const button = document.querySelector(".sort-btn");
+
+button.addEventListener("click", () => {
+  courses.sort((a, b) => a.price - b.price);
+  generateLIST();
+});
+
+const button2 = document.querySelector(".sort-rev-button");
+
+button2.addEventListener("click", () => {
+  courses.sort((a, b) => b.price - a.price);
+  generateLIST();
+});
