21762b61f96b1fc3232bd8d36394352cb5900954
Sourav <sourav.choudhury@positra.com>
2023-03-16 15:30:29 +0530

    Inheritance in Javascript

--- 06 AdvanceIsh/06classjs.js
+++ 06 AdvanceIsh/06classjs.js
@@ -4,16 +4,44 @@ class User {
     this.email = email;
   }
 
-  courseList = [];
+  #courseList = [];
+
   getInfo() {
     return { name: this.name, email: this.email };
   }
   enrollCourse(name) {
-    this.courseList.push(name);
+    this.#courseList.push(name);
   }
   getCourseList() {
-    return this.courseList;
+    return this.#courseList;
+  }
+
+  login() {
+    return `You are logged in`;
+  }
+}
+
+class subAdmin extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+  getAdminInfo() {
+    return "Iam sub-admin";
+  }
+  login() {
+    return "login for admin only";
   }
 }
 
 module.exports = User;
+
+const rock = new User("rock", "rock@rock.com");
+// console.log(rock.getInfo());
+rock.enrollCourse("Angular Boot camp");
+// console.log(rock.getCourseList());
+// console.log(rock.courseList);
+
+const tom = new subAdmin("tom", "tom@jerry.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+console.log(tom.getInfo());
