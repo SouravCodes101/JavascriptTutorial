209fba87f59b943a602c58b63019570d5573c729
Sourav Choudhury <62167988+SouravCodes101@users.noreply.github.com>
2023-03-19 20:39:37 +0530

    ES12

---
+++ ES12/index.js
@@ -0,0 +1,21 @@
+// const str = "Do yahoo people use yahoo search";
+// const newStr = str.replaceAll("yahoo", "Yahoo!");
+// console.log(newStr);
+
+// const rejectMe = new Promise((_, reject) => reject("path issues"));
+// const resolveMe = new Promise((resolve) => resolve());
+
+// Promise.any([rejectMe, resolveMe, rejectMe])
+//   .then(() => console.log("handle resolve"))
+//   .catch((e) => console.log("Rejected"));
+
+// function cartTotal(value) {
+//   value ??= 0;
+//   console.log("Your total is: ", value);
+// }
+
+// cartTotal();
+// cartTotal(30);
+
+num = 100_000;
+console.log(num);
